17900a843b71bb94e2bc40413d191b9a1e76f71b
Mia Tian <miatian@Mias-MBP.fios-router.home>
2021-01-26 17:29:25 -0500

    calendar

--- app/src/main/java/com/example/vhlapp/CalendarFragment.java
+++ app/src/main/java/com/example/vhlapp/CalendarFragment.java
@@ -9,12 +9,57 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.util.Log;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import java.text.DateFormatSymbols;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import java.util.Calendar;
 
 public class CalendarFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        // handle add button
+        FloatingActionButton addButton = view.findViewById(R.id.floatingActionButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                v.setEnabled(false);
+            }
+        });
+        CalendarView calender = view.findViewById(R.id.calendar);
+        TextView dateView = view.findViewById(R.id.dateView);
+
+        //set textview
+        String date = formatDate();
+        dateView.setText(date);
+
+        // Add Listener in calendar
+        calender.setOnDateChangeListener(
+                        new CalendarView.OnDateChangeListener() {
+                            @Override
+                            public void onSelectedDayChange(@NonNull CalendarView view,
+                                    int year, int month, int dayOfMonth)
+                            {
+                                formatDate();
+                                String monthString = new DateFormatSymbols().getMonths()[month];
+                                String date = monthString + " " + dayOfMonth + ", " + year;
+                                dateView.setText(date);
+                            }
+                        });
+
+        return view;
     }
+
+    public String formatDate(){
+        Calendar now = Calendar.getInstance();
+        String month = new DateFormatSymbols().getMonths()[now.get(Calendar.MONTH)];
+        return month + " " + now.get(Calendar.DATE) + ", " + now.get(Calendar.YEAR);
+    }
+
+
+
 }
