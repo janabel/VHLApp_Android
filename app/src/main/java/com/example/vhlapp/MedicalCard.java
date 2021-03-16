23b5a0fe6033e697f5e47117d314bbe568054334
Janabel Xia <janabeltxia@gmail.com>
2021-03-16 19:28:31 -0400

    Medical card

---
+++ app/src/main/java/com/example/vhlapp/MedicalCard.java
@@ -0,0 +1,72 @@
+package com.example.vhlapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+import com.shockwave.pdfium.PdfDocument;
+
+import java.util.List;
+
+public class MedicalCard extends Activity implements OnPageChangeListener, OnLoadCompleteListener {
+    private static final String TAG = HandbookPDF.class.getSimpleName();
+    public static final String SAMPLE_FILE = "ER_card.pdf";
+
+    PDFView pdfView;
+    Integer pageNumber = 0;
+    String pdfFileName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handbook_p_d_f);
+        setTitle("Handbook PDF");
+
+        Intent p = getIntent();
+        int page = p.getIntExtra("pageNumber", 0);
+
+        pdfView = (PDFView)findViewById(R.id.pdfView);
+        pdfView.fromAsset(SAMPLE_FILE)
+                .defaultPage(page-1) //because of 0-indexing
+                .enableSwipe(true)
+                .swipeHorizontal(true)
+                .onPageChange(this)
+                .enableAnnotationRendering(true)
+                .onLoad(this)
+                .scrollHandle(new DefaultScrollHandle(this))
+                .pageSnap(true)
+                .load();
+
+    }
+
+    @Override
+    public void onPageChanged(int page, int pageCount) {
+        pageNumber = page;
+        setTitle(String.format("%s %s / %s", pdfFileName, page + 1, pageCount));
+    }
+
+
+    @Override
+    public void loadComplete(int nbPages) {
+        PdfDocument.Meta meta = pdfView.getDocumentMeta();
+        printBookmarksTree(pdfView.getTableOfContents(), "-");
+
+    }
+
+    public void printBookmarksTree(List<PdfDocument.Bookmark> tree, String sep) {
+        for (PdfDocument.Bookmark b : tree) {
+
+            Log.e(TAG, String.format("%s %s, p %d", sep, b.getTitle(), b.getPageIdx()));
+
+            if (b.hasChildren()) {
+                printBookmarksTree(b.getChildren(), sep + "-");
+            }
+        }
+    }
+
+}
