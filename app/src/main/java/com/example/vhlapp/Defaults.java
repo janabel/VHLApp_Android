87d98eb33f757338b3ce475ac51bdb34608f38ad
Janabel Xia <janabeltxia@gmail.com>
2022-10-05 21:49:43 -0400

    remove keys

--- app/src/main/java/com/example/vhlapp/Defaults.java
+++ app/src/main/java/com/example/vhlapp/Defaults.java
@@ -8,10 +8,6 @@ public class Defaults
     public static final String API_KEY = config.API_KEY;
     public static final String SERVER_URL = "https://api.backendless.com";
 =======
-    public static final String APPLICATION_ID = "";
-    public static final String API_KEY = "";
-    public static final String SERVER_URL = "https://api.backendless.com";
-    
 >>>>>>> 3b1f2dccf4d24076245cccb05dbc534801da7174
 
 }
