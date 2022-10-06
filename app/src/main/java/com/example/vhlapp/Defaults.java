3b1f2dccf4d24076245cccb05dbc534801da7174
janabel <74808097+janabel@users.noreply.github.com>
2022-10-05 21:31:35 -0400

    Update Defaults.java

--- app/src/main/java/com/example/vhlapp/Defaults.java
+++ app/src/main/java/com/example/vhlapp/Defaults.java
@@ -3,12 +3,9 @@ package com.example.vhlapp;
 public class Defaults
 {
 
-    public static final String APPLICATION_ID = "A0009585-9151-DFCE-FF47-F2191066F400";
-    public static final String API_KEY = "836F2B37-DC25-2C8F-FF81-31CB12B97C00";
+    public static final String APPLICATION_ID = "";
+    public static final String API_KEY = "";
     public static final String SERVER_URL = "https://api.backendless.com";
-//
-//    public static final String APPLICATION_ID = "8BDC2255-6F5F-EBA0-FF6E-60686A950A00";
-//    public static final String API_KEY = "F9FAC3D3-696E-451B-BA27-E735D5D3ED3D";
-//    public static final String SERVER_URL = "https://api.backendless.com";
+    
 
 }
